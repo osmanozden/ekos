7ecd3ecf1ac69f9b7f2cfc41d223720c2099e616
osmanozden <ozden.osman@hotmail.com>
2022-12-07 19:06:18 +0300

    first com

---
+++ src/main/java/com/assessment/ekos/EkosApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.ekos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/ekos/EkosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.ekos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
