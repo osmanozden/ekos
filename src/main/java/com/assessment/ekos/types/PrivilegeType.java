5b237a3a54d72e42a20d0dbeea5ed3783fdeab43
osmanozden <ozden.osman@hotmail.com>
2022-12-08 01:10:35 +0300

    CRUD Completed

---
+++ src/main/java/com/assessment/ekos/types/PrivilegeType.java
@@ -0,0 +1,1 @@
+package com.assessment.ekos.types;import lombok.Getter;@Getterpublic enum PrivilegeType {    READ("READ"),    DELETE("DELETE"),    UPDATE("UPDATE"),    CREATE("CREATE"),    APPROVE("APPROVE");    private String name;    PrivilegeType(String text) {        this.name = text;    }    public String getName() {        return name;    }}
