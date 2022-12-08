a4831d0c941ee52c54926bce50988e301144f248
osmanozden <ozden.osman@hotmail.com>
2022-12-08 18:52:08 +0300

    Remove scanpagages

--- src/main/java/com/assessment/ekos/EkosApplication.java
+++ src/main/java/com/assessment/ekos/EkosApplication.java
@@ -3,7 +3,7 @@ package com.assessment.ekos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.assessment.ekos")
+@SpringBootApplication
 public class EkosApplication {
 
     public static void main(String[] args) {
